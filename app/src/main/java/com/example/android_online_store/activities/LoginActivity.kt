5034048961b6116ce9bdc12bb217604a305e88b2
sorinradneantu <62335059+sorinradneantu@users.noreply.github.com>
2021-11-14 18:00:40 +0200

    Register UI created

---
+++ app/src/main/java/com/example/android_online_store/activities/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.android_online_store.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.android_online_store.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val goToRegisterButton = findViewById<Button>(R.id.goToRegisterButton)
+        goToRegisterButton.setOnClickListener { goToRegister() }
+
+    }
+
+    private fun goToRegister() {
+
+        val registerActivity = Intent(this, RegisterActivity::class.java).apply {
+
+        }
+        startActivity(registerActivity)
+        finish()
+    }
+
+}
