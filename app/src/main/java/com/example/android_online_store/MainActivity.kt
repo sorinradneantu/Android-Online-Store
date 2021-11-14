1f0c7a1cad2a2433776414489416a240d3c9a004
sorinradneantu <62335059+sorinradneantu@users.noreply.github.com>
2021-11-14 17:34:26 +0200

    Project created

---
+++ app/src/main/java/com/example/android_online_store/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android_online_store
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
