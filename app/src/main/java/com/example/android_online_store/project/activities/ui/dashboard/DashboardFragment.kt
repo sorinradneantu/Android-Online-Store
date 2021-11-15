c7a6df02350df9eb5549c65dcedd46fbb035598c
sorinradneantu <62335059+sorinradneantu@users.noreply.github.com>
2021-11-15 21:14:54 +0200

    settings button added in Dashboard activity

--- app/src/main/java/com/example/android_online_store/project/activities/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/android_online_store/project/activities/ui/dashboard/DashboardFragment.kt
@@ -1,18 +1,26 @@
 package com.example.android_online_store.project.activities.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.example.android_online_store.R
 import com.example.android_online_store.databinding.FragmentDashboardBinding
+import com.example.android_online_store.project.activities.SettingsActivity
 
 class DashboardFragment : Fragment() {
 
     //private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+
     private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
@@ -34,10 +42,32 @@ class DashboardFragment : Fragment() {
             textView.text = "This is dashboard Fragment"
 
         return root
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        when(id){
+            R.id.action_settings -> {
+
+                startActivity(Intent(activity, SettingsActivity::class.java))
+
+                return true
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/example/android_online_store/project/activities/ui/home/ProductsFragment.kt
+++ app/src/main/java/com/example/android_online_store/project/activities/ui/home/ProductsFragment.kt
@@ -8,12 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.android_online_store.databinding.FragmentHomeBinding
+import com.example.android_online_store.databinding.FragmentProductsBinding
 
-class HomeFragment : Fragment() {
+
+class ProductsFragment : Fragment() {
 
     //private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentProductsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -26,7 +27,7 @@ class HomeFragment : Fragment() {
     ): View? {
         //homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentProductsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
